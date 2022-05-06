47a8c727438c7590e12c1c999c3625a297010923
kimdaniels1999 <86605048+kimdaniels1999@users.noreply.github.com>
2022-05-06 07:21:03 -0700

    Enpoints and Router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const v1Router = require("./v1/routes");
+// const v1Router = require("./v1/routes");
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000; 
@@ -10,7 +11,9 @@ const PORT = process.env.PORT || 3000;
 //     res.send("<h2> It's working!</h2>");
 // });
 
-app.use("/api/v1", v1Router);
+// app.use("/api/v1", v1Router);
+
+app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
     console.log(`API is listening on port ${PORT}`);
