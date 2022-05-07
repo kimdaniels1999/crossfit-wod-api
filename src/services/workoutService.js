23ebdb2bbf6037bad5371966991fb07fa0c71a4b
kimdaniels1999 <86605048+kimdaniels1999@users.noreply.github.com>
2022-05-07 03:24:33 -0700

    Create service layer
    Name service methods same as controller methods to have a connection.
    Controller layer now talks with service layer.

---
+++ src/services/workoutService.js
@@ -0,0 +1,28 @@
+const getAllWorkouts = () => {
+    return;
+};
+
+const getOneWorkout = () => {
+    return;
+};
+
+const createNewWorkout = () => {
+    return;
+};
+
+const updateOneWorkout = () => {
+    return;
+};
+
+const deleteOneWorkout = () => {
+    return;
+};
+
+module.exports = {
+    getAllWorkouts,
+    getOneWorkout,
+    createNewWorkout,
+    updateOneWorkout,
+    deleteOneWorkout,
+};
+
