e3eaf754577e728d5bfd39586128b0196f0a603a
kimdaniels1999 <86605048+kimdaniels1999@users.noreply.github.com>
2022-05-07 02:05:00 -0700

    refactor router use controller methods
    test endpoint once done - localhost:3000/api/v1/workouts/2342
    The first layer of our architecture is done

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,24 +1,36 @@
 const express = require("express");
+const workoutController = require("../../controllers/workoutController");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Get all workouts");
-});
+router.get("/", workoutController.getAllWorkouts);
 
-router.get("/:workoutId", (req, res) => {
-    res.send("Get an existing workout");
-});
+// router.get("/", (req, res) => {
+//     res.send("Get all workouts");
+// });
 
-router.post("/", (req, res) => {
-    res.send("Create a new workout");
-});
+router.get("/:workoutId", workoutController.getOneWorkout);
 
-router.patch("/:workoutId", (req, res) => {
-    res.send("Update an existing workout");
-});
+// router.get("/:workoutId", (req, res) => {
+//     res.send("Get an existing workout");
+// });
 
-router.delete("/:workoutId", (req, res) => {
-    res.send("Delete an existing workout");
-});
+router.post("/", workoutController.createNewWorkout);
+
+// router.post("/", (req, res) => {
+//     res.send("Create a new workout");
+// });
+
+router.patch("/:workoutId", workoutController.updateOneWorkout);
+
+// router.patch("/:workoutId", (req, res) => {
+//     res.send("Update an existing workout");
+// });
+
+router.delete("/:workoutId", workoutController.deleteOneWorkout);
+
+// router.delete("/:workoutId", (req, res) => {
+//     res.send("Delete an existing workout");
+// });
 
 module.exports = router;
